b9104ebd7679b4c93d00371f34af5599e6e18d4a
Knoxx04 <onyiknoxx3@gmail.com>
2023-03-22 14:35:46 +0300

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "destroying..."
+
+Restaurant.destroy_all
+Pizza.destroy_all
+RestaurantPizza.destroy_all
+
+puts "seeding..."
+5.times do
+    name = Faker::Name.name
+    address= Faker::Address.full_address
+    Restaurant.create(name:name, address:address)
+    5.times do
+      Pizza.create(
+        name: Faker::Food.dish,
+        ingredients: Faker::Food.ingredient
+      )
+    end
+    5.times do
+      # Create a random Restaurant and Pizza to associate
+      restaurant = Restaurant.order("RANDOM()").first
+      pizza = Pizza.order("RANDOM()").first
+      # Create a new RestaurantPizza
+      restaurant_pizza = RestaurantPizza.new(
+        price: Faker::Commerce.price(range: 5..20.0, as_string: true),
+        pizza_id: pizza.id,
+        restaurant_id: restaurant.id
+      )
+      # Save the new RestaurantPizza to the database
+      restaurant_pizza.save
+    end
+end
+
+puts "done seeding"
