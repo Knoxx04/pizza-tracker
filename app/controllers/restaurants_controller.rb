a03e3c4b91564b692d361631848c4e99a9434838
Knoxx04 <onyiknoxx3@gmail.com>
2023-03-22 14:45:18 +0300

    restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,24 @@
 class RestaurantsController < ApplicationController
-end
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    
+
+    def index
+        restaurants = Restaurant.all 
+        render json: restaurants.as_json(except: :pizzas)
+    end
+
+    def show
+        restaurant = Restaurant.includes(:pizzas).find(params[:id])
+        render json: restaurant, include: :pizzas
+    end
+
+    def destroy
+        restaurant = Restaurant.find(params[:id])
+        restaurant.destroy
+        head:no_content
+    end
+
+    def render_not_found_response
+        render json: { error: "Restaurant not found" }, status: :not_found
+      end
+    end
